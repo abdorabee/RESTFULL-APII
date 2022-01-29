0f04d4914a7723bcb6e36cc14eba62d163411602
Abdel-Rahaman Rabee <abdorabee1134@gmail.com>
2022-01-29 13:57:57 +0200

    fixed some errors

--- index.js
+++ index.js
@@ -4,26 +4,23 @@
  */
 
 // Dependencies
-var server = require('./lib/server');
-var workers = require('./lib/workers');
+var server = require("./lib/server");
+var workers = require("./lib/workers");
 
 // Declare the app
 var app = {};
 
 // Init function
-app.init = function(){
-
+app.init = function () {
   // Start the server
   server.init();
 
   // Start the workers
   workers.init();
-
 };
 
 // Self executing
 app.init();
-
 
 // Export the app
 module.exports = app;
