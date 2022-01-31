bc7acd01f923c1ce9d41452ba5ef29f2c815d1f2
Abdel-Rahaman Rabee <abdorabee1134@gmail.com>
2022-01-31 23:29:21 +0200

    added some error scripting to test

--- index-debug.js
+++ index-debug.js
@@ -14,18 +14,47 @@ var app = {};
 // Init function
 app.init = function () {
   // Start the server
+  debugger;
   server.init();
+  debugger;
 
   // Start the workers
+  debugger;
   workers.init();
+  debugger;
 
   // Start the CLI , but make sure it starts last
+  debugger;
   setTimeout(function () {
     cli.init();
+    debugger;
   }, 50);
+  debugger;
 
+  // Start an script that has issues (throws an error)
+  debugger;
+  // Set foo at 1
+  var foo = 1;
+  console.log("Just assigned 1 to foo");
+  debugger;
+
+  // Increment foo
+  foo++;
+  console.log("Just increamented foo");
+  debugger;
+
+  // Square foo
+  foo = foo * foo;
+  console.log("Just squared foo");
+  debugger;
+
+  // Convert foo to a string
+  foo = foo.toString();
+  console.log("Just converted foo to string");
+  debugger;
   // Call the init script that will throw
   debuggingProblem.init();
+  debugger;
 };
 
 // Self executing
